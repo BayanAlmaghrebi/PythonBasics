edf9bcf4c8a04650e174f26c241a47856113fda1
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-07-03 21:20:49 +0200

    range

---
+++ 434.py
@@ -0,0 +1,259 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:\Users\bayan\Desktop\my codes\434py.py ==============
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+=============== RESTART: C:\Users\bayan\Desktop\my codes\434py.py ==============
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+=============== RESTART: C:\Users\bayan\Desktop\my codes\434py.py ==============
+0
+1
+2
+3
+4
+5
+
+=============== RESTART: C:\Users\bayan\Desktop\my codes\434py.py ==============
+1
+2
+3
+4
+5
+
+=============== RESTART: C:\Users\bayan\Desktop\my codes\434py.py ==============
+1
+2
+1
+2
+1
+2
+1
+2
+1
+2
+
+=============== RESTART: C:\Users\bayan\Desktop\my codes\434py.py ==============
+1 1
+1 2
+2 1
+2 2
+3 1
+3 2
+4 1
+4 2
+5 1
+5 2
+
+=============== RESTART: C:\Users\bayan\Desktop\my codes\434py.py ==============
+p
+y
+t
+h
+o
+n
+
+=============== RESTART: C:\Users\bayan\Desktop\my codes\434py.py ==============
+ahmed
+ali
+mohamed
+
+=============== RESTART: C:\Users\bayan\Desktop\my codes\434py.py ==============
+1
+2
+3
+4
+5
+
+Warning (from warnings module):
+  File "C:\Users\bayan\Desktop\my codes\434py.py", line 21
+    for x in [1,2 ,3,4,5[1,2,3,4]]:
+SyntaxWarning: 'int' object is not subscriptable; perhaps you missed a comma?
+>>> 
+=============== RESTART: C:\Users\bayan\Desktop\my codes\434py.py ==============
+Traceback (most recent call last):
+  File "C:\Users\bayan\Desktop\my codes\434py.py", line 21, in <module>
+    for x in [1,2 ,3,4,5[1,2,3,4]]:
+TypeError: 'int' object is not subscriptable
+
+=============== RESTART: C:\Users\bayan\Desktop\my codes\434py.py ==============
+1
+2
+3
+4
+5
+[1, 2, 3, 4]
+
+range(10)
+range(0, 10)
+
+list(range(10))
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+list(range(3,10))
+[3, 4, 5, 6, 7, 8, 9]
+
+list (range(3,10,2))
+[3, 5, 7, 9]
+
+=============== RESTART: C:\Users\bayan\Desktop\my codes\434py.py ==============
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+=============== RESTART: C:\Users\bayan\Desktop\my codes\434py.py ==============
+1
+2
+3
+4
+5
+
+=============== RESTART: C:\Users\bayan\Desktop\my codes\434py.py ==============
+1 1
+1 2
+1 3
+1 4
+1 5
+1 6
+1 7
+1 8
+1 9
+1 10
+1 11
+1 12
+2 1
+2 2
+2 3
+2 4
+2 5
+2 6
+2 7
+2 8
+2 9
+2 10
+2 11
+2 12
+3 1
+3 2
+3 3
+3 4
+3 5
+3 6
+3 7
+3 8
+3 9
+3 10
+3 11
+3 12
+4 1
+4 2
+4 3
+4 4
+4 5
+4 6
+4 7
+4 8
+4 9
+4 10
+4 11
+4 12
+5 1
+5 2
+5 3
+5 4
+5 5
+5 6
+5 7
+5 8
+5 9
+5 10
+5 11
+5 12
+
+=============== RESTART: C:\Users\bayan\Desktop\my codes\434py.py ==============
+1 X 1 = 1
+1 X 2 = 2
+1 X 3 = 3
+1 X 4 = 4
+1 X 5 = 5
+1 X 6 = 6
+1 X 7 = 7
+1 X 8 = 8
+1 X 9 = 9
+1 X 10 = 10
+1 X 11 = 11
+1 X 12 = 12
+2 X 1 = 2
+2 X 2 = 4
+2 X 3 = 6
+2 X 4 = 8
+2 X 5 = 10
+2 X 6 = 12
+2 X 7 = 14
+2 X 8 = 16
+2 X 9 = 18
+2 X 10 = 20
+2 X 11 = 22
+2 X 12 = 24
+3 X 1 = 3
+3 X 2 = 6
+3 X 3 = 9
+3 X 4 = 12
+3 X 5 = 15
+3 X 6 = 18
+3 X 7 = 21
+3 X 8 = 24
+3 X 9 = 27
+3 X 10 = 30
+3 X 11 = 33
+3 X 12 = 36
+4 X 1 = 4
+4 X 2 = 8
+4 X 3 = 12
+4 X 4 = 16
+4 X 5 = 20
+4 X 6 = 24
+4 X 7 = 28
+4 X 8 = 32
+4 X 9 = 36
+4 X 10 = 40
+4 X 11 = 44
+4 X 12 = 48
+5 X 1 = 5
+5 X 2 = 10
+5 X 3 = 15
+5 X 4 = 20
+5 X 5 = 25
+5 X 6 = 30
+5 X 7 = 35
+5 X 8 = 40
+5 X 9 = 45
+5 X 10 = 50
+5 X 11 = 55
+5 X 12 = 60
---
+++ 434py.py
@@ -0,0 +1,37 @@
+'''
+x = 5
+y = 6
+z = 7
+
+if all([x==5 , y==6 , z==7]):
+    print ('-----')
+'''
+
+'''
+x = 1
+while x <= 5 :
+    y = 1
+    while y < 3 :
+        print(x,y)
+        y += 1
+    x += 1
+
+'''
+'''
+for x in [1,2,3,4,5,[1,2,3,4]]:
+    print (x)
+'''
+
+
+range(10)
+
+
+range(3,10)
+
+
+range(3,10,2)
+
+for x in range (1,6):
+    for y in range (1,13):
+      
+        print(f"{x} X {y} = {x*y}")
