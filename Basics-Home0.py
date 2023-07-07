d133938cc0d6d613430c46b60a1c262ab633397b
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-07-07 20:54:56 +0200

    add Basics1

---
+++ Basics-Home0.py
@@ -0,0 +1,160 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x = 5
+x = 10
+if = 5
+SyntaxError: invalid syntax
+
+f = 'mystro'
+
+type(x)
+<class 'int'>
+
+type(f)
+<class 'str'>
+
+x = 2.0
+type(x)
+<class 'float'>
+
+f = 'mahmoud'
+l = [1,2,3,True , False]
+t = (1,2,3,4,5)
+d = {'ahmed':2 , 'ali':30}
+
+k = [5]
+type(k)
+<class 'list'>
+
+r = ('mystro')
+type(r)
+<class 'str'>
+s = ('mystro',)
+type(s)
+<class 'tuple'>
+
+f= 't'
+g = "t"
+k = '''t'''
+f
+'t'
+g
+'t'
+k
+'t'
+2+3
+5
+3*4
+12
+2**5
+32
+9/3
+3.0
+
+9//3
+3
+9/4
+2.25
+9//4
+2
+
+9%4
+1
+
+x = 5
+x == 5
+True
+x == 6
+False
+y==5
+Traceback (most recent call last):
+  File "<pyshell#46>", line 1, in <module>
+    y==5
+NameError: name 'y' is not defined
+x > 6
+False
+x = 5
+x < 6
+True
+x != 6
+True
+x >= 10
+False
+
+x = 5
+x = 6
+x = x + 1
+x
+7
+
+x += 1
+x
+8
+
+x *= 3
+x
+24
+
+x /= 8
+x
+3.0
+x **=4
+x
+81.0
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics0.py ======
+x > 400
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics0.py ======
+x > 400
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics0.py ======
+------
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics0.py ======
+x > 100
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics0.py ======
+x > 100
+welcome
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics0.py ======
+x > 100
+welcome
+>>> 
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics0.py ======
+x > 100
+welcome
+welcome 2
+>>> 
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics0.py ======
+welcome
+welcome 2
+>>> 
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics0.py ======
+x>30
+>>> 
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics0.py ======
+x>30
+>>> 
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics0.py ======
+x>30
+x>30
+>>> 
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics0.py ======
+-----
+>>> 
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics0.py ======
+>>> 
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics0.py ======
+-----
+>>> 
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics0.py ======
+----
+----
+>>> 
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics0.py ======
+----
+----
+----
+----
---
+++ Basics0.py
@@ -0,0 +1,15 @@
+x = 5
+y = 6
+z = 7
+
+if x==5 and y==6 and z==7:
+     print('----')
+
+if all([x==5 , y==6 , z==7]):
+     print('----')
+
+if x==5 or y==6 or z==7:
+     print('----')
+
+if any([x==5 , y==6 , z==7]):
+    print('----')
