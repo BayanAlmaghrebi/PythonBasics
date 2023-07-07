b6915d1483cbb98c1993449f294ab4a0040f877c
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-07-07 19:54:41 +0200

    mysum

---
+++ Basics.py
@@ -0,0 +1,80 @@
+
+'''
+    - required
+    - keyword = positional
+    - default
+    - variable length
+
+'''
+
+'''
+# defination
+def mysum(y,x=0):  # arguments , parameters
+        print(x+y)
+
+# call
+mysum(5)
+'''
+
+
+'''
+result = 5
+for i in (10, 4, 3, 4, 5, 6, 7, 8, 19):
+        result += i
+        print(result)
+'''
+
+
+
+
+
+'''
+
+def mysum(x,y):  
+        return x+y
+
+# call
+g = mysum(5,10)
+print(g*2)
+
+'''
+
+'''
+f = 0
+print(f)
+
+def do():
+    global f
+    f = 5
+    print(f)
+
+do()
+
+print(f)
+
+'''
+
+'''
+def mysum(x,y):
+    return x+y
+
+print(mysum(5,10))
+
+mysum2 = lambda x,y:x+y
+print(mysum2(5,10))
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
