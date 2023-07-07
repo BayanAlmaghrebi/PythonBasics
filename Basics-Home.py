63789b9b9b8b00d325aa4d8ccdc77579d87f92b5
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-07-07 19:56:41 +0200

    add PythonHome

---
+++ Basics-Home.py
@@ -0,0 +1,290 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+15
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+15
+115
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+115
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+15
+115
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+15
+15
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+15
+115
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+50
+1500
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+Traceback (most recent call last):
+  File "C:/Users/bayan/Desktop/my codes/python Basics/Basics.py", line 16, in <module>
+    mysum(5)
+TypeError: mysum() missing 1 required positional argument: 'y'
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+15
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+15
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+Traceback (most recent call last):
+  File "C:/Users/bayan/Desktop/my codes/python Basics/Basics.py", line 16, in <module>
+    mysum(y=5)
+TypeError: mysum() missing 1 required positional argument: 'x'
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+Traceback (most recent call last):
+  File "C:/Users/bayan/Desktop/my codes/python Basics/Basics.py", line 16, in <module>
+    mysum()
+TypeError: mysum() missing 2 required positional arguments: 'x' and 'y'
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+0
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+5
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+20
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+15
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+5
+(10, 4)
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+5
+(10, 4, 3, 4, 5, 6, 7, 8, 19)
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+15
+19
+22
+26
+31
+37
+44
+52
+71
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+15
+19
+22
+26
+31
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+15
+19
+22
+26
+31
+
+for x in (10, 4, 3, 4, 5, 6, 7, 8, 19):
+    print(x)
+
+    
+10
+4
+3
+4
+5
+6
+7
+8
+19
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+15
+19
+22
+26
+31
+37
+44
+52
+71
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+15
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+0
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+5
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+50
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+0
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+5
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+6
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+Traceback (most recent call last):
+  File "C:/Users/bayan/Desktop/my codes/python Basics/Basics.py", line 16, in <module>
+    mysum()
+TypeError: mysum() missing 1 required positional argument: 'y'
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+5
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+Traceback (most recent call last):
+  File "C:/Users/bayan/Desktop/my codes/python Basics/Basics.py", line 61, in <module>
+    mysum(5.10)
+TypeError: mysum() missing 1 required positional argument: 'y'
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+15
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+15
+None
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+15
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+30
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+0
+0
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+0
+5
+0
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+0
+5
+5
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+15
+
+======= RESTART: C:/Users/bayan/Desktop/my codes/python Basics/Basics.py =======
+15
+15
+(lambda x,y:x+y)(5,10)
+15
+
+d= (lambda x,y:x+y)(5,10)
+
+
+d= (lambda x,y:x+y)(5,10)
+
+c =
+SyntaxError: incomplete input
+c = (lambda x,y:x+y)(5,10)
+c
+15
+d
+15
+
+
+x = 'm'
+f="m"
+g='''m'''
+x
+'m'
+f
+'m'
+m
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    m
+NameError: name 'm' is not defined
+g
+'m'
+x*5
+'mmmmm'
+x+x
+'mm'
+fname = 'mahmoud'
+lnam = 'ahmed'
+print(f"my full name is {fname}{lname}")
+Traceback (most recent call last):
+  File "<pyshell#26>", line 1, in <module>
+    print(f"my full name is {fname}{lname}")
+NameError: name 'lname' is not defined. Did you mean: 'fname'?
+print(f"my full name is {fname}{lnam}")
+my full name is mahmoudahmed
+print(f"my full name is {fname} {lnam}")
+my full name is mahmoud ahmed
+f = f"my full name is {fname} {lnam}"
+f
+'my full name is mahmoud ahmed'
+f.title()
+'My Full Name Is Mahmoud Ahmed'
+f.upper()
+'MY FULL NAME IS MAHMOUD AHMED'
+f.lower()
+'my full name is mahmoud ahmed'
+f.islower()
+True
+f.isupper()
+False
+>>> f.istitle()
+False
+>>> f.replace('ahmed','ali')
+'my full name is mahmoud ali'
+>>> 
+>>> f.split(' ')
+['my', 'full', 'name', 'is', 'mahmoud', 'ahmed']
+>>> 
+>>> c = [1,2,3,4,5,True,'ahmed']
+>>> c[0]
+1
+>>> 
+>>> c[-1]
+'ahmed'
+>>> 
+>>> c[2:5]
+[3, 4, 5]
+>>> 
+>>> c[0:4]
+[1, 2, 3, 4]
+>>> c[:4]
+[1, 2, 3, 4]
+>>> 
+>>> c[4:]
+[5, True, 'ahmed']
+>>> 
+>>> c = [1,2,3,4,5,True,'ahmed']
+>>> c[0]
+1
+>>> c[0]=100
+>>> c
+[100, 2, 3, 4, 5, True, 'ahmed']
+>>> 
+>>> c.append(1000)
+>>> c
+[100, 2, 3, 4, 5, True, 'ahmed', 1000]
+>>> c.insert(0,500)
+>>> c
+[500, 100, 2, 3, 4, 5, True, 'ahmed', 1000]
