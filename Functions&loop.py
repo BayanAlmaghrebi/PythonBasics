1ac00150063ebc97135d929edead1c75fa0e9cf0
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-07-10 11:00:56 +0200

    added functions

---
+++ Functions&loop.py
@@ -0,0 +1,4 @@
+d = {'ahmed':70 , 'ali':80}
+
+for x,y in d.items():
+    print(x,y)
---
+++ Functions.py
@@ -0,0 +1,16 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> l = [1,2,3,4,5,6]
+>>> l[0]
+1
+>>> l[-1]
+6
+>>> l[-2]
+5
+>>> 
+>>> l[0:3]
+[1, 2, 3]
+>>> l[4:]
+[5, 6]
+>>> l[:3]
+[1, 2, 3]
